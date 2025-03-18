368eec30bc684c55fecfd74d87b4e8f31092cf1e
山城李香 <>
2025-03-18 20:56:07 +0900

    no message

--- app/src/main/java/com/example/momolog/data/api/ApiService.java
+++ app/src/main/java/com/example/momolog/data/api/ApiService.java
@@ -1,9 +1,20 @@
 package com.example.momolog.data.api;
 
+import com.example.momolog.data.model.Category;
+import com.example.momolog.data.model.StoreInfo;
+
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 
-//public interface ApiService {
-//    @GET("/greeting")
-//    //Call<Greeting> getGreeting();
-//}
+public interface ApiService {
+
+    //カテゴリー一覧
+    @GET("/api/v1/category/list")
+    Call<List<Category>> getCategoryListAll();
+
+    //店舗情報一覧
+    @GET("/api/v1/storeinfo/list")
+    Call<List<StoreInfo>> getStoreInfoListAll();
+}
